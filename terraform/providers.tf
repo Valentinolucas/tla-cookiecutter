ed1c8533fd4b290e9b5206fd774e6d0dd56d01b3
Lucas Fantini <valentinolucasfantini@gmail.com>
2023-11-10 13:26:20 +0000

    feat(CI): rename pipelines

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -4,9 +4,6 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket  = "developement-tf-state"
-    key     = "tla-example/dev_plan.tfstate"
-    region  = "eu-west-2" # Set your desired region
-    encrypt = true
+
   }
 }

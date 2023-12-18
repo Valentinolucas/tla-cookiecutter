c8ac24878de84fb91b8728ea8ef5a026fbe35781
Lucas Fantini <valentinolucasfantini@gmail.com>
2023-12-18 15:43:15 +0000

    ci: ammend python version

---
+++ get-docker.sh
@@ -0,0 +1,15 @@
+HTTP/1.1 200 OK
+Content-Type: text/plain
+Content-Length: 21927
+Connection: keep-alive
+Last-Modified: Mon, 11 Sep 2023 15:28:13 GMT
+x-amz-version-id: yKQxlXGdj38mTO87kJ7Yrc7KQ_Kaf9oo
+Server: AmazonS3
+Date: Mon, 18 Dec 2023 04:49:42 GMT
+ETag: "21991ad5158db696823e3fd54eab00da"
+X-Cache: Hit from cloudfront
+Via: 1.1 fcb7e2ea03344c5cbdb3ce71f1ffa1dc.cloudfront.net (CloudFront)
+X-Amz-Cf-Pop: LHR50-P6
+X-Amz-Cf-Id: BE8xKVYftZdwyPq--VeJlac76TM7wKo0aKB6E0Ia9MuK0UMkmrWuZw==
+Age: 33166
+
